17c9e797b086c4a6fe9afd932a2b25abb3891f5c
hyu1335 <hyuham1335@gmail.com>
2026-03-03 15:11:34 +0900

    refactor: todo 검색 JPQL을 Querydsl로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("SELECT t " +
             "FROM Todo t LEFT JOIN FETCH t.user u " +
@@ -19,9 +19,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             "AND (:endDate IS NULL OR t.modifiedAt <= :endDate)" +
             "ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderWithModifiedAtAndWeather(LocalDateTime startDate, LocalDateTime endDate, String weather, Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
